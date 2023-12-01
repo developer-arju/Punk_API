9116150dd5871dd89822feff8abe3529bfc9e4dc
developer-arju <arjunsajeevan369@gmail.com>
2023-12-02 03:00:04 +0530

    updation on fetch function

--- src/utils/BeerService.ts
+++ src/utils/BeerService.ts
@@ -3,7 +3,8 @@ import { ROOT_ENDPOINT, BEER_ENDPOINT, PER_PAGE } from "./Constants"
 
 export const fetchBeers = async (filter: boolean): Promise<[] | any> => {
     try {
-        const response = await fetch(`${ROOT_ENDPOINT}${BEER_ENDPOINT}?${filter ? "abv_gt=8" : "abv_lt=8"}`)
+        const query: string = `${filter ? "abv_gt=8" : "abv_lt=8"}`
+        const response = await fetch(`${ROOT_ENDPOINT}${BEER_ENDPOINT}?${query}`)
         if(!response.ok) throw new Error('network error')
 
         return await response.json()
@@ -15,7 +16,8 @@ export const fetchBeers = async (filter: boolean): Promise<[] | any> => {
 
 export const fetchCurrPageBeers = async (page: number, filter: boolean): Promise<[] | any> => {
     try {
-        const response = await fetch(`${ROOT_ENDPOINT}${BEER_ENDPOINT}?page=${page}&per_page=${PER_PAGE}&${filter ? "abv_gt=8" : "abv_lt=8"}`)
+        const query: string = `page=${page}&per_page=${PER_PAGE}&${filter ? "abv_gt=8" : "abv_lt=8"}`
+        const response = await fetch(`${ROOT_ENDPOINT}${BEER_ENDPOINT}?${query}`)
         if(!response.ok) throw new Error('network error')
 
         return await response.json()
